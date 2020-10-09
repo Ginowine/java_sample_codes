c85653ee5036235659478f27154d0cb8e07511bc
ginowine <ginowine1@gmail.com>
2020-10-09 22:37:28 +0100

    DataTypes

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -29,3 +29,23 @@ public class DataTypes {
         }
     }
 }
+
+//YOU CAN ALSO USE THE BELOW SAMPLE CODE TO ACHIEVE THE ABOVE FUNCTIONALITY
+
+// try
+//         {
+//         long x=sc.nextLong();
+//         System.out.println(x+" can be fitted in:");
+//         if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE){
+//         System.out.println("* byte");
+//         }
+//         if (x>=Short.MIN_VALUE && x<=Short.MAX_VALUE){
+//         System.out.println("* short");
+//         }
+//         if (x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE){
+//         System.out.println("* int");
+//         }
+//         if (x>=Long.MIN_VALUE && x<=Long.MAX_VALUE){
+//         System.out.println("* long");
+//         }
+//         }
