ad3275bb80ac8928c6366adb7c91e607f26b2351
ginowine <ginowine1@gmail.com>
2020-10-09 19:49:26 +0100

    loop/formatting

--- src/Solution.java
+++ src/Solution.java
@@ -4,7 +4,6 @@ public class Solution {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
-
         for(int i=0;i<t;i++){
             int a = in.nextInt();
             int b = in.nextInt();
