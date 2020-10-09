23151fc244e54330aa15258e30fffc858706d3ab
ginowine <ginowine1@gmail.com>
2020-10-09 15:28:05 +0100

    loop/formatting

--- src/Looping.java
+++ src/Looping.java
@@ -8,6 +8,9 @@ public class Looping {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         scanner.close();
 
+        for(int i = 1; i <= 10; i++){
 
+            System.out.printf("%d x %d = %d%n", N, i, N*i);
+        }
     }
 }
