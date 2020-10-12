ecd7dc2919e54b6a887cae8fe8fcb69d9e8d903e
ginowine <ginowine1@gmail.com>
2020-10-12 16:37:41 +0100

    stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,15 +1,32 @@
 import java.util.Scanner;
 
-public class Stack {
+public class Stack<C> {
     public static void main(String []argh)
     {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
             String input=sc.next();
-            //Complete the code
-            while(input.length() != (input = input.replaceAll("\\(\\)|\\[\\]|\\{\\}", "")).length());
-            System.out.println(input.isEmpty());
+            java.util.Stack<Character> stacky = new java.util.Stack<Character>();
+            for (int i = 0; i < input.length(); i++) {
+                if (!stacky.isEmpty()) {
+                    switch(input.charAt(i)) {
+                        case '}' : if (stacky.peek() == '{') {
+                            stacky.pop();
+                        } break;
+                        case ']' : if (stacky.peek() == '[') {
+                            stacky.pop();
+                        } break;
+                        case ')' : if (stacky.peek() == '(') {
+                            stacky.pop();
+                        } break;
+                        default: stacky.push(input.charAt(i));
+                    }
+                } else {
+                    stacky.push(input.charAt(i));
+                }
+            }
+            System.out.println(stacky.isEmpty());
         }
 
     }
