a76a7463cc27567ca156175799061fadf6a1425d
ginowine <ginowine1@gmail.com>
2020-10-12 10:44:46 +0100

    StaticInitializerBlock

--- src/StaticInitializerBlock.java
+++ src/StaticInitializerBlock.java
@@ -5,5 +5,14 @@ public class StaticInitializerBlock {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+        int B = scanner.nextInt();
+        int H = scanner.nextInt();
+
+        if (B <= 0 || H <= 0){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }else {
+            int area = B * H;
+            System.out.println(area);
+        }
     }
 }
