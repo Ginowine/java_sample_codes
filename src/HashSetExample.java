f318080fd1f2ed64c21910ef0791af6cde630541
ginowine <ginowine1@gmail.com>
2020-10-13 20:53:08 +0100

    Hashset

--- src/HashSetExample.java
+++ src/HashSetExample.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class HashSetExample {
@@ -9,7 +10,18 @@ public class HashSetExample {
         h.add("India");
         h.add("Australia");
         h.add("Nigeria");
+        h.add("India");
 
+        System.out.println(h);
 
+        h.remove("Australia");
+        System.out.println("Set after removing " + "Australia:" + h);
+
+        System.out.println("Iterating over set:");
+
+        Iterator<String> i = h.iterator();
+        while (i.hasNext()){
+            System.out.println(i.next());
+        }
     }
 }
