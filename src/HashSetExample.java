df7877832a833b217abf847104cc0af0ab866b6c
ginowine <ginowine1@gmail.com>
2020-10-13 20:30:22 +0100

    Hashset

---
+++ src/HashSetExample.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+
+    public static void main(String[] args){
+        Set<String> h = new HashSet<String>();
+
+        h.add("India");
+        h.add("Australia");
+        h.add("Nigeria");
+
+
+    }
+}
