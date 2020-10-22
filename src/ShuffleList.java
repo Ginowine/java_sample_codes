bd8ee4f6b5a26303739123da1b93eee329a18848
ginowine <ginowine1@gmail.com>
2020-10-22 14:22:08 +0100

    shuffle using a list

--- src/ShuffleList.java
+++ src/ShuffleList.java
@@ -6,8 +6,10 @@ import java.util.Random;
 public class ShuffleList {
 
     public static void main(String[] args){
-        List<String> list = new ArrayList<String>();
-        for (String a : args){
+
+        int[] data = {2, 4, 3, 1, 6, 8, 5};
+        List<Integer> list = new ArrayList<Integer>();
+        for (int a : data){
             list.add(a);
             Collections.shuffle(list, new Random());
             System.out.println(list);
