399186ad7054b450673165efd10f63484c82bd21
ginowine <ginowine1@gmail.com>
2020-10-25 08:28:58 +0100

    binary searching a list passing the list and element as an argument.

---
+++ src/SearchingList.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SearchingList {
+
+    public static void main(String[] args){
+        List<String> list = new LinkedList<>();
+        list.add("element 2");
+        list.add("element 1");
+        list.add("element 4");
+        list.add("element 3");
+
+        Collections.sort(list);
+        for (String str : list) {
+            System.out.println(" sort elements in ascending order  --" + str);
+        }
+
+        int index = Collections.binarySearch(list, "element 4");
+        System.out.println("Element found at ::" + index);
+    }
+}
