e538e6a019f0fecf8477ccb7fd78329b8829c2e1
ginowine <ginowine1@gmail.com>
2020-10-26 13:56:08 +0100

    filling a range of an array with a value

--- src/FillingAnArray.java
+++ src/FillingAnArray.java
@@ -4,8 +4,11 @@ public class FillingAnArray {
     public static void main(String[] args){
         int[] array = {1, 2, 3,4,5,6,7,8,8,9};
         Arrays.fill(array, 10);
-        System.out.println("Array completely filled" +
-                " with 10\n" + Arrays.toString(array));
+//        System.out.println("Array completely filled" +
+//                " with 10\n" + Arrays.toString(array));
+        for (int a : array){
+            System.out.println("Value = " + a);
+        }
 
         fillASubArray(array, 1, 5, 6);
 
@@ -14,7 +17,6 @@ public class FillingAnArray {
 
     public static void fillASubArray(int[] in, int fromIndex, int toIndex, int value){
         Arrays.fill(in, fromIndex, toIndex, value);
-
         System.out.println(Arrays.toString(in));
     }
 }
